8f35b0cc48183e3623e471c4607f88729c0e1048
duynguyenminh84 <minhduy.nguyen@my.jcu.edu.au>
2024-05-26 20:25:31 +1000

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,32 @@
+"""
+Question a
+"for i in range(0, 101, 10):
+    print(i, end=' ')
+print()
+"""
+
+"""
+Question b
+for i in reversed(range(1, 21, 1)):
+    print(i, end=' ')
+print()
+"""
+
+"""
+Question c
+print("How many * do you want ?")
+stars= int(input("Enter stars: "))
+for i in range(0, stars, 1):
+    print("*", end=' ')
+print()
+"""
+
+"""
+Question d
+print("How many * do you want ?")
+stars= int(input("Enter stars: "))
+for i in range(0, stars + 1):
+    print("*" * i)
+print()
+
+"""
